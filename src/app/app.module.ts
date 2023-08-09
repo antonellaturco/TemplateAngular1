4e79a039dcb9f45207a2ec4711f05d998e397753
aturco <antonellaturco01@gmail.com>
2023-08-09 16:42:23 -0300

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ServicesComponent } from './services/services.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { WhyUsComponent } from './why-us/why-us.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ClientsComponent } from './clients/clients.component';
+import { TeamComponent } from './team/team.component';
+import { NuestrosClientesComponent } from './nuestros-clientes/nuestros-clientes.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ServicesComponent,
+    AboutUsComponent,
+    WhyUsComponent,
+    PortfolioComponent,
+    ClientsComponent,
+    TeamComponent,
+    NuestrosClientesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
